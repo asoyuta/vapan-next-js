bfc71315ef27eb16d92efff732ec42a03f27cff3
asoyuta <asoyuta@icloud.com>
2022-01-02 18:27:33 +0900

    edit chat display

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -17,7 +17,7 @@ function Article({ title, createDate, updateDate, description, lines }) {
           {/* Option / Character Details / Stream Details */}
           <div className=" bg-sky-900 h-14 top-0 z-50"></div>
           {/* Chat */}
-          <div className="bg-blue-200 h-[calc(100vh-96px)] overflow-scroll scrollbar-hide px-2 py-3">
+          <div className="bg-blue-200 h-[calc(90vh-96px)] overflow-scroll scrollbar-hide px-2 py-3">
             {lines.map(({ displayName, icon, nihongo, furigana, freeEN }, i) => (
               <Line key={i} displayName={displayName} icon={icon} nihongo={nihongo} freeEN={freeEN} furigana={furigana} />
             ))}
@@ -26,6 +26,7 @@ function Article({ title, createDate, updateDate, description, lines }) {
           <div className="bg-slate-500 h-10 top-0 z-50"></div>
         </div>
       </div>
+      <div className='h-screen'></div>
     </div>
   )
 }
