ae54e5b4e422481bd1a4f7a744e1d8b8a09c0c5b
asoyuta <asoyuta@icloud.com>
2022-01-05 21:27:53 +0900

    chat

--- src/components/UIkit/Toggle.js
+++ src/components/UIkit/Toggle.js
@@ -2,9 +2,13 @@ function Toggle({ toggled, onClick }) {
   return (
     <div
       onClick={onClick}
-      className={`relative cursor-pointer m-auto h-6 w-12 bg-gray-400 rounded-full ${toggled && 'bg-lime-500'}`}
+      className={`relative cursor-pointer m-auto h-6 w-12 rounded-full ${toggled ? 'bg-lime-500' : 'bg-gray-400'}`}
     >
-      <div className={`absolute top-0.5 left-0.5 h-5 w-5 bg-white rounded-full transition-all duration-300 ease-in-out ${toggled && 'translate-x-6'}`} />
+      <div
+        className={`absolute top-0.5 left-0.5 h-5 w-5 bg-white rounded-full transition-all duration-300 ease-in-out ${
+          toggled && 'translate-x-6'
+        }`}
+      />
     </div>
   )
 }
